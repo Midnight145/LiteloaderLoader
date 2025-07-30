0642836d4660727e50fededc3b62edeadda15e36
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-07-30 18:00:50 -0500

    spotless

---
+++ src/main/java/com/midnight/liteloaderloader/core/transformers/compat/VoxelCommonLiteModTransformer.java
@@ -0,0 +1,7 @@
+package com.midnight.liteloaderloader.core.transformers.compat;
+
+import com.midnight.liteloaderloader.core.transformers.ClassTransformer;
+
+public class MineLPLinuxCompat extends ClassTransformer {
+    
+}
