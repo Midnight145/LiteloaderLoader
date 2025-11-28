88ec9853dca65ab3979bc1112f64bc6e10778c1f
Midnight145 <23142346+Midnight145@users.noreply.github.com>
2025-11-28 17:01:59 -0600

    MacroKeybindMod + Controlling compat (#21)
    * add modified R reflection lib
    
    * macrokeybind + controlling compat
    
    * use WeakReference with null checks to further ensure class objects can be garbage collected

---
+++ src/main/java/com/midnight/liteloaderloader/core/transformers/compat/MacroModCoreTransformer.java
@@ -0,0 +1,36 @@
+package com.midnight.liteloaderloader.core.transformers.compat;
+
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import com.midnight.liteloaderloader.core.transformers.ClassTransformer;
+
+public class MacroModCoreTransformer extends ClassTransformer {
+
+    public MacroModCoreTransformer() {
+        super();
+        this.methodTransforms.put("transformKeyBindings", this::transformTransformKeyBindings);
+    }
+
+    private void transformTransformKeyBindings(MethodNode methodNode) {
+        //
+        int idx = 0;
+        for (AbstractInsnNode insn : methodNode.instructions.toArray()) {
+            if (insn.getOpcode() == INVOKEVIRTUAL) {
+                if (idx == 1) {
+                    MethodInsnNode newInsn = new MethodInsnNode(
+                        INVOKESTATIC,
+                        "com/midnight/liteloaderloader/core/runtime/RuntimeMethods",
+                        "getKeyBindingEntries",
+                        "(Lnet/minecraft/client/gui/GuiKeyBindingList;)[Lnet/minecraft/client/gui/GuiListExtended$IGuiListEntry;",
+                        false);
+                    methodNode.instructions.set(insn, newInsn);
+                    return;
+                }
+                idx++;
+            }
+        }
+    }
+
+}
